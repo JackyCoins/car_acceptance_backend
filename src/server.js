4f0f8fe53d3a5f510ccb2923fefdfd7b6c879960
Nikita Rusanov <rusanov.n.d@gmail.com>
2018-10-20 19:14:13 +0300

    Added package.json and .gitignore

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
+//region Import libraries
 const http = require('http');
+//endregion
 
 const port = 8080;
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
+  console.log(request.url);
   response.end('Hello world');
 };
 
 const server = http.createServer(requestHandler);
 
-server.listen(port, (err) => {
+server.listen(port, err => {
   if (err) {
     return console.log('something bad happened', err);
   }
 
   console.log(`server is listening on ${port}`);
-})
+});
